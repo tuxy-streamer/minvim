bf8e153d43c9af518554712b0bbbdbce005004fd
tuxy-streamer <subham020304@gmail.com>
2026-01-11 22:49:06 +0530

    refactor: remove unnecessary default code

--- lua/tuxy.lua
+++ lua/tuxy.lua
@@ -538,23 +538,6 @@ require("render-markdown").setup({
 		border_prefix = false,
 		above = "",
 		below = "",
-		backgrounds = {
-			"RenderMarkdownH1Bg",
-			"RenderMarkdownH2Bg",
-			"RenderMarkdownH3Bg",
-			"RenderMarkdownH4Bg",
-			"RenderMarkdownH5Bg",
-			"RenderMarkdownH6Bg",
-		},
-		foregrounds = {
-			"RenderMarkdownH1",
-			"RenderMarkdownH2",
-			"RenderMarkdownH3",
-			"RenderMarkdownH4",
-			"RenderMarkdownH5",
-			"RenderMarkdownH6",
-		},
-		custom = {},
 	},
 })
 
